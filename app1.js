5d5f67d32c41050f9ed9ea50bf39497bc5a1c45b
Éphraïm <ephreycleophace@gmail.com>
2018-08-29 03:06:41 +0200

    cardio all file

---
+++ app1.js
@@ -0,0 +1,26 @@
+//var libraryName = 'Lib 1';
+/*
+var persone = new Object();
+
+persone['firstName'] = "Ephraim";
+persone['lastName'] = "Ilunga";
+
+ var firstNameProperty = 'firstName';
+
+ console.log(persone);
+ console.log(persone[firstNameProperty]);
+
+ console.log(persone.firstName);
+ console.log(persone.lastName);
+
+ persone.address = new Object();
+ persone.address.street = '37 Grundel Road Glenmore';
+ persone.address.city = 'Durban';
+ persone.address.province = 'Kwazulu-Natal';
+
+ console.log(persone.address.street);
+ console.log(persone.address.city);
+ console.log(persone.address.province);
+ console.log(persone['address']['street']);
+
+ */
---
+++ app2.js
@@ -0,0 +1,29 @@
+//window.libraryName = window.libraryName || 'Lib 2';
+/*
+var Ephraim = {
+    firstName: 'Ephraim', 
+    lastName: 'Ilunga',
+    address: {
+        street: '37 Grundel Road',
+        city: 'Durban'
+    }
+};
+
+function greet(person) {
+    console.log('Hi ' + person.firstName);
+}
+
+greet(Ephraim);
+
+greet({
+    firstName: 'Marc',
+    lastName: 'Ilunga'
+});
+
+Ephraim.address2 = {
+    street: '38 Grundel Road'
+};
+
+console.log(Ephraim);
+*/
+//store procedure
